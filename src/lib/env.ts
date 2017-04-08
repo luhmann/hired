0e66de6004ef9e66801ce7f36c176999a2a7a6dd
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-04-08 16:28:34 +0200

    chore(config): Fix config

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,7 @@
 const DEV_MODE_IDENTIFIER = 'dev'
 
-const BACKEND_BASE_URL = `${process.env.BACKEND_PROTOCOL}://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}`
+const BACKEND_BASE_URL = 
+  `${process.env.REACT_APP_BACKEND_PROTOCOL}://${process.env.REACT_APP_BACKEND_HOST}:${process.env.REACT_APP_BACKEND_PORT}`
 
 const backendUrl = (fragment: string): string => {
   return `${BACKEND_BASE_URL}${fragment}`
--- src/storage/firebaseRepository.ts
+++ src/storage/firebaseRepository.ts
@@ -5,11 +5,11 @@ import { backendUrl } from '../lib/env'
 
 class FirebaseRepository {
   private config = Object.freeze({
-    apiKey: `${process.env.FB_API_KEY}`,
-    authDomain: `${process.env.FB_AUTH_DOMAIN}`,
-    databaseURL: `${process.env.FB_DATABASE_URL}`,
-    storageBucket: `${process.env.FB_STORAGE_BUCKET}`,
-    messagingSenderId: `${process.env.FB_MESSAGING_SENDER_ID}`
+    apiKey: `${process.env.REACT_APP_FB_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_FB_AUTH_DOMAIN}`,
+    databaseURL: `${process.env.REACT_APP_FB_DATABASE_URL}`,
+    storageBucket: `${process.env.REACT_APP_FB_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_FB_MESSAGING_SENDER_ID}`
   })
 
   private app: firebase.app.App
