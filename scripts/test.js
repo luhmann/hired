a1833c477372ab8aeb8478adff3e011f33457966
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-05-03 08:21:40 +0200

    fix: More tweaks to pre-push hook

--- scripts/test.js
+++ scripts/test.js
@@ -12,10 +12,5 @@ require('dotenv').config({silent: true});
 const jest = require('jest');
 const argv = process.argv.slice(2);
 
-// Watch unless on CI or in coverage mode
-if (!process.env.CI && argv.indexOf('--coverage') < 0 && argv.indexOf('--once') < 0) {
-  argv.push('--watch');
-}
-
 
 jest.run(argv);
